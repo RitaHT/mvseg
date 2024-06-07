e357262413434867b9f89a85bbc7be059c6c6d02
RitaSQTan <35492829+RitaHT@users.noreply.github.com>
2024-06-07 16:45:23 -0400

    checking transforms

--- trial_timm.py
+++ trial_timm.py
@@ -9,8 +9,22 @@ import pandas as pd
 import timm
 import torch
 
-model = timm.create_model("hf_hub:timm/mobilevit_xxs.cvnets_in1k", pretrained=True)
+import torchvision.transforms as trans
 
-x = torch.randn(1, 3, 224, 224) #pretrained weights need 3 channels
 
-print(model(x).shape) #torch.Size([1,1000])
+# Feature map
+m2 = timm.create_model(
+    'mobilevitv2_050.cvnets_in1k', 
+    features_only=True, 
+    pretrained=True)
+
+# get model specific transforms (normalization, resize)
+data_config = timm.data.resolve_model_data_config(m2)
+transforms = timm.data.create_transform(**data_config, is_training=False)
+
+# torchvision transforms module takes PIL image with 3 channels, need to add lines
+pil_image = trans.ToPILImage()(slice_T2)
+x = transforms(pil_image).unsqueeze(0) #torch.Size([1, 3, 256, 256])
+print("x.shape = {}".format(x.shape))
+output = m2(x) #list with len=5 tensors
+
